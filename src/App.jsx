f8854b0c40a0e79bd934dadb2fee96bd709f541d
astrum <astrum@iMac-astrum-261.local>
2025-04-25 18:20:01 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Basic from "./Companents/Basic"
+
+
+
+function App() {
+  return (
+    <>
+    <div>
+    <Basic/>
+      </div>
+      </>
+  )
+}
+
+export default App
